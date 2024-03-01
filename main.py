ea85d1f488cacc81db8aa0050e901823405c3fca
David Augulis <33178822+Augudav@users.noreply.github.com>
2024-03-01 14:46:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -134,7 +134,7 @@ if dashboard == 'Past Power Law':
     fig.update_layout(height=800, width=1000,  showlegend=True)
     fig.update_yaxes(title_text="Price ($)", row=1, col=1)
     fig.update_yaxes(title_text="Difference (%)", row=2, col=1)
-    fig.update_xaxes(title_text="Date", row=2, col=1)
+    #fig.update_xaxes(title_text="Date", row=2, col=1)
 
     if chart_type == "Linear":
         fig.update_layout(xaxis_title='Date', yaxis_title='Price', xaxis_rangeslider_visible=False)
