38ef034b71bd63918bacea288fb1f9d325f933c7
David Augulis <33178822+Augudav@users.noreply.github.com>
2024-03-01 14:52:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -240,13 +240,17 @@ if dashboard == 'Future Power Law':
 
 
 if dashboard == 'Risk Visualization':
-    # Load in the data for the dash
-    #st.title(f'{instrument} Risk Visualization')
-
-    col1, col2, col3 = st.columns([1,6,1])  # Adjust the ratio to better center the title
+    col1, col2, col3 = st.columns(3)
+    
+    with col1:
+        st.write(' ')
     
     with col2:
         st.title(f'{instrument} Risk Visualization')
+    
+    with col3:
+        st.write(' ')
+
     
     chart_type = st.sidebar.select_slider(
         'Select scale type',
