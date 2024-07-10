634d751550d58711c6ca9b819f036d35c9e4e8eb
mariusortiz <122994686+mariusortiz@users.noreply.github.com>
2024-07-10 21:36:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -183,59 +183,6 @@ def plot_risk_visualization(df, instrument):
 
     st.plotly_chart(fig, use_container_width=True)
 
-def plot_past_power_law(df, instrument):
-    st.markdown(f"<h2 style='text-align: center;'>{instrument} Historical Power Law Predictions</h2>", unsafe_allow_html=True)
-    
-    chart_type = st.sidebar.select_slider(
-        'Select scale type',
-        options=['Linear', 'Logarithmic'],
-        value="Linear"
-    )
-    
-    max_date_with_close = df.dropna(subset=['close'])['date'].max()
-    df = df[df["date"] <= max_date_with_close]
-    
-    fig = make_subplots(rows=2, cols=1, shared_xaxes=True, vertical_spacing=0.1,
-                        subplot_titles=(f'Actual vs Predicted Prices - {instrument}', 'Percentage Difference between Actual and Historical Predicted Prices'))
-    fig.add_trace(go.Scatter(x=df['date'], y=df['close'], mode='lines', name='Actual Price'), row=1, col=1)
-    fig.add_trace(go.Scatter(x=df['date'], y=df['predicted_next_day_price'], mode='lines', name='Predicted Next Day Price', line=dict(color='cyan')), row=1, col=1)
-    
-    fig.add_annotation(
-        text="KASPING.STREAMLIT.APP",
-        align='left',
-        opacity=0.4,
-        font=dict(color="red", size=35),
-        xref='paper',
-        yref='paper',
-        x=0.5,
-        y=0.5,
-        showarrow=False
-    )
-    
-    differences = 100 * (df['close'] - df['predicted_next_day_price']) / df['predicted_next_day_price']
-    fig.add_trace(go.Scatter(x=df['date'], y=differences, mode='lines', name='Difference (%)'), row=2, col=1)
-    fig.add_hline(y=0, line=dict(dash='dash', color='red'), row=2, col=1)
-
-    fig.update_layout(height=800, width=1000, showlegend=True)
-    fig.update_yaxes(title_text="Price ($)", row=1, col=1)
-    fig.update_yaxes(title_text="Difference (%)", row=2, col=1)
-    fig.update_xaxes(title_text="Date", row=2, col=1)
-
-    if chart_type == "Linear":
-        fig.update_layout(yaxis_title='Price', xaxis_rangeslider_visible=False)
-    elif chart_type == "Logarithmic":
-        fig.update_layout(yaxis=dict(type='log', title='Price'), xaxis_rangeslider_visible=False)
-
-    st.plotly_chart(fig, use_container_width=True)
-    expander = st.expander('About the chart')
-    expander.write('''
-    You might find it surprising to see the predicted value fluctuate. Typically, power law charts depict the fair price as a constant, straight line (on log-log charts) because they are curve-fitted on the past data for the best fit.
-
-    However, this doesn't reveal past predictions, which is crucial for assessing the reliability of these forecasts.
-
-    This chart is designed differently. It shows predictions as they would have been made using all available data at each point in the past. The goal is to demonstrate the degree to which power law predictions can vary, giving you insight into their consistency.
-    ''')
-
 # Fonction pour tracer le graphique Future Power Law
 def plot_future_power_law(df, instrument):
     # Calculer la différence de jours entre aujourd'hui et la date maximale des données
@@ -284,17 +231,6 @@ def plot_future_power_law(df, instrument):
     elif chart_type == "Logarithmic":
         fig.update_layout(xaxis_title='Date', yaxis=dict(type='log', title='Price'), xaxis_rangeslider_visible=False)
 
-    fig.add_annotation(
-        text="KASPING.STREAMLIT.APP",
-        align='left',
-        opacity=0.4,
-        font=dict(color="red", size=35),
-        xref='paper',
-        yref='paper',
-        x=0.5,
-        y=0.5,
-        showarrow=False,
-    )
 
     st.plotly_chart(fig, use_container_width=True)
     expander = st.expander('About the chart')
@@ -306,37 +242,71 @@ def plot_future_power_law(df, instrument):
     This chart is designed differently. It shows predictions as they would have been made using all available data at each point in the past. The goal is to demonstrate the degree to which power law predictions can vary, giving you insight into their consistency.
     ''')
 
-# Utilisation de la fonction
-csv_file = 'kas_d.csv'
-df = pd.read_csv(csv_file)
-df['date'] = pd.to_datetime(df['date'])
-df = calculate_predicted_price(df)
-plot_future_power_law(df, 'Kaspa (KAS)')
+# Fonction pour tracer le graphique Past Power Law
+def plot_past_power_law(df, instrument):
+    st.markdown(f"<h2 style='text-align: center;'>{instrument} Historical Power Law Predictions</h2>", unsafe_allow_html=True)
 
-
-def main():
-    st.set_page_config(layout="wide")
-
-    csv_file = 'kas_d.csv'
-    df = pd.read_csv(csv_file)
-    df['date'] = pd.to_datetime(df['date'])
-
-    instrument = "Kaspa (KAS)"
-    df = calculate_predicted_price(df)
-
-    dashboard = st.sidebar.selectbox(
-        label='Select dashboard',
-        options=['Rainbow chart', 'Risk Visualization', 'Past Power Law', 'Future Power Law']
+    chart_type = st.sidebar.select_slider(
+        'Select scale type',
+        options=['Linear', 'Logarithmic'],
+        value="Linear"
     )
 
-    if dashboard == 'Rainbow chart':
-        plot_rainbow_chart(df, instrument)
-    elif dashboard == 'Risk Visualization':
-        plot_risk_visualization(df, instrument)
-    elif dashboard == 'Past Power Law':
-        plot_past_power_law(df, instrument)
-    elif dashboard == 'Future Power Law':
-        plot_future_power_law(df, instrument)
+    # Create subplots
+    fig = make_subplots(rows=2, cols=1, shared_xaxes=True, vertical_spacing=0.1,
+                        subplot_titles=(f'Actual vs Predicted Prices - {instrument}', 'Percentage Difference between Actual and Historical Predicted Prices'))
+    fig.add_trace(go.Scatter(x=df['date'], y=df['close'], mode='lines', name='Actual Price'), row=1, col=1)
+    fig.add_trace(go.Scatter(x=df['date'], y=df['predicted_next_day_price'], mode='lines', name='Predicted Next Day Price', line=dict(color='white')), row=1, col=1)
+
+    differences = 100 * (df['close'] - df['predicted_next_day_price']) / df['predicted_next_day_price']
+    fig.add_trace(go.Scatter(x=df['date'], y=differences, mode='lines', name='Difference (%)'), row=2, col=1)
+    fig.add_hline(y=0, line=dict(dash='dash', color='red'), row=2, col=1)
+
+    # Update layout
+    fig.update_layout(height=800, width=1000, showlegend=True)
+    fig.update_yaxes(title_text="Price ($)", row=1, col=1)
+    fig.update_yaxes(title_text="Difference (%)", row=2, col=1)
+    fig.update_xaxes(title_text="Date", row=2, col=1)
+
+    if chart_type == "Linear":
+        fig.update_layout(yaxis_title='Price', xaxis_rangeslider_visible=False)
+    elif chart_type == "Logarithmic":
+        fig.update_layout(yaxis=dict(type='log', title='Price'), xaxis_rangeslider_visible=False)
+
+    st.plotly_chart(fig, use_container_width=True)
+    expander = st.expander('About the chart')
+    expander.write('''
+    You might find it surprising to see the predicted value fluctuate. Typically, power law charts depict the fair price as a constant, straight line (on log-log charts) because they are curve-fitted on the past data for the best fit.
+    
+    However, this doesn't reveal past predictions, which is crucial for assessing the reliability of these forecasts.
+    
+    This chart is designed differently. It shows predictions as they would have been made using all available data at each point in the past. The goal is to demonstrate the degree to which power law predictions can vary, giving you insight into their consistency.
+    ''')
+
+# Chargement des données CSV
+csv_file = 'kas_d.csv'  # Remplacez par le chemin de votre fichier CSV
+df = pd.read_csv(csv_file)
+df['date'] = pd.to_datetime(df['date'])
+
+# Calculer les prix prédits
+df = calculate_predicted_price(df)
+
+instrument = "Kaspa (KAS)"  # Remplacez par l'instrument souhaité si nécessaire
+
+dashboard = st.sidebar.selectbox(
+    label='Select dashboard',
+    options=['Rainbow chart', 'Risk Visualization', 'Past Power Law', 'Future Power Law']
+)
+
+# Affichage des graphiques en fonction de la sélection de l'utilisateur
+if dashboard == 'Rainbow chart':
+    plot_rainbow_chart(df, instrument)
+elif dashboard == 'Risk Visualization':
+    plot_risk_visualization(df, instrument)
+elif dashboard == 'Past Power Law':
+    plot_past_power_law(df, instrument)
+elif dashboard == 'Future Power Law':
+    plot_future_power_law(df, instrument)
 
 if __name__ == "__main__":
     main()
